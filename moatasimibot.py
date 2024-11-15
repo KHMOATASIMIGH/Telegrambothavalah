892693df7fcff4fdbafa54d88a7d2eaa6e6eaee4
KHMOATASIMIGH <168250152+KHMOATASIMIGH@users.noreply.github.com>
2024-11-15 15:15:32 +0430

    Update moatasimibot.py

--- moatasimibot.py
+++ moatasimibot.py
@@ -69,7 +69,7 @@ async def cancel(update: Update, context: CallbackContext):
 
 # تنظیم ربات
 def main():
-    application = Application.builder().token("TELEGRAM_TOKEN_BOT").build()
+    application = Application.builder().token("TTB").build()
 
     conversation_handler = ConversationHandler(
         entry_points=[CommandHandler("start", start)],
